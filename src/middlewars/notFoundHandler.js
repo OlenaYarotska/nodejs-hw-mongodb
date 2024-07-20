7ed3562d4cef42f9a052b5e95d0cc7142b54957c
Olena Yarotska <olena.zhurba@gmail.com>
2024-07-20 21:39:00 +0300

    added new files

---
+++ src/middlewars/notFoundHandler.js
@@ -0,0 +1,32 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+    if (err instanceof HttpError) {
+        res.status(err.status).json({
+            status: err.status,
+            message: err.name,
+            data: err,
+        });
+        return;
+    }
+    res.status(500).json({
+       status: 500,
+		message: "Something went wrong",
+		data: err.message,
+    });
+};
+
+
+export const notFoundHandler = (err, req, res, next) => {
+    if (err instanceof HttpError) {
+        res.status(err.status).json({
+            status: err.status,
+            message: err.name,
+            data: err,
+        });
+        return;
+    }
+    res.status(404).json({
+        message: 'Route not found',
+    });
+};
